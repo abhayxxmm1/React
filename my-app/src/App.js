6135535d87b3e7d621cee60be118eec627dc72fe
Abhay <ghongadeabhay60@gmail.com>
2024-12-06 21:50:38 +0530

    Day 1 Basics completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -45,8 +45,30 @@ function secondFunction() {
     </div>
   );
 }
+function thirdFunction() {
+  // component is a funciont that returns u a HTML like syntax called JSX ( javascript xml language ) ( HTML like syntax )
+
+  return (
+    <div className="SecondFunction">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 // export default secondFunction; // cannot have more than one default export
-export { secondFunction} // this is valid
+export { secondFunction, thirdFunction} // this is valid
 
 export default App; // exporting this app component from here and we can use it any where else
