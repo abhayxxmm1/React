026bd052de249e85ae1722c286b9a3dd031397e8
Abhay <ghongadeabhay60@gmail.com>
2024-12-06 20:35:25 +0530

    1:08:00  day 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg'; // Import statement
-import './App.css';
+import logo from "./logo.svg"; // Import statement
+import "./App.css";
 
-function App() { // component is a funciont that returns u a HTML like syntax called JSX ( javascript xml language ) ( HTML like syntax ) 
+function App() {
+  // component is a funciont that returns u a HTML like syntax called JSX ( javascript xml language ) ( HTML like syntax )
 
   return (
     <div className="App">
@@ -22,5 +23,30 @@ function App() { // component is a funciont that returns u a HTML like syntax ca
     </div>
   );
 }
+function secondFunction() {
+  // component is a funciont that returns u a HTML like syntax called JSX ( javascript xml language ) ( HTML like syntax )
 
-export default App;
+  return (
+    <div className="SecondFunction">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+// export default secondFunction; // cannot have more than one default export
+export { secondFunction} // this is valid
+
+export default App; // exporting this app component from here and we can use it any where else
--- my-app/src/Notes.md
+++ my-app/src/Notes.md
@@ -15,6 +15,24 @@ function
     define
 */
 // component is a funciont that returns u a HTML like syntax called JSX ( javascript xml language ) ( HTML like syntax ) 
-// Import statement
+// Import statementnpx 
+// src is the main folder where all the react code will be located
+// component's are the fucntion's with additonal power's
+/* 
+function
+    call a function
+    pass parameters
+    arguments
+    resues
+    return value
+    set of instructions
+    block of code
+    define
+*/
+// component is a funciont that returns u a HTML like syntax called JSX ( javascript xml language ) ( HTML like syntax ) 
 
+// npx & npm dont have official naming but called as node package manager
+
+// Their are two types of export
+// export default (max 1) & another is normal export 
 ```
