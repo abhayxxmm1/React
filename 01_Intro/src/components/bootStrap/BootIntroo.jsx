a28675eb956b6a4e0eca5ad424f974b6418f71eb
Abhay <ghongadeabhay60@gmail.com>
2026-01-22 11:43:02 +0530

    Boot

---
+++ 01_Intro/src/components/bootStrap/BootIntroo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ImgOverlayExample from './bootComponents/ImgOverlayExample'
+
+const BootIntroo = () => {
+  return (
+    <div>
+        <h1>
+        BootIntroo
+        </h1>
+        <ImgOverlayExample/>
+        <paginationBasic/>
+
+    </div>
+  )
+}
+
+export default BootIntroo
