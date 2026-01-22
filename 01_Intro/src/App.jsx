db014db7b2838e251be03be37f13153815063fb6
Abhay <ghongadeabhay60@gmail.com>
2026-01-22 11:43:29 +0530

    TailWind CSS

--- 01_Intro/src/App.jsx
+++ 01_Intro/src/App.jsx
@@ -1,12 +1,26 @@
+import BootIntroo from "./components/bootStrap/BootIntroo";
+import EventIntro from "./components/events/EventIntro";
 import Intro from "./components/intro/Intro";
+import JsxIntroo from "./components/jsx/JsxIntroo";
+import MapIntro from "./components/map/MapIntro";
+import PropsInto from "./components/props/PropsInto";
 import StateIntro from "./components/state/StateIntro";
+import TailWIntro from "./components/tailwindCss/TailWIntro";
 
 let App = ()=>{
   return(
     <div>
       <h1>App</h1>
     {/* <Intro/> */}
-    <StateIntro/>
+    {/* <StateIntro/> */}
+    {/* <PropsInto/> */} {/* need to clear  */}
+    {/* <JsxIntroo/> */}
+    {/* <MapIntro/> */}
+    {/* <BootIntroo/> */}
+    <TailWIntro/>
+    <EventIntro/>
+   
+
     </div>
   )
 }
---
+++ 01_Intro/src/components/tailwindCss/TailWIntro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TailWIntro = () => {
+  return (
+    <div>TailWIntro
+        <h1>Hello</h1>
+        <p>Hello</p>
+    </div>
+  )
+}
+
+export default TailWIntro
